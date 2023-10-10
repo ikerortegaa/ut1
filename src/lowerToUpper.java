afea1cb6c1d0e864747893e761444f06160b1a09
ikerortegaa <ikerortegaberna@gmail.com>
2023-10-10 23:24:53 +0200

    Ejercicio 3 loweToUpper termiando.

--- src/lowerToUpper.java
+++ src/lowerToUpper.java
@@ -1,7 +1,9 @@
 /*Hacer ejercicio que cambie minúsculas a mayúsculas y lo copie en otro fichero*/
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,37 +11,37 @@ import java.nio.file.StandardOpenOption;
 
 public class lowerToUpper {
     public static void main(String[] args) {
-        Path path = Paths.get("lower.txt");
-        try(FileChannel fcl= FileChannel.open(path, StandardOpenOption.READ);
-            FileChannel fce= FileChannel.open(Paths.get("upper.txt"),StandardOpenOption.WRITE,StandardOpenOption.CREATE);) {
-
-            ByteBuffer bb = ByteBuffer.allocate(1);
-
-
-            //Leer del canal de lectura para escribir en el buffer y desde el buffer escribir al canal de escritura
-
-            while (fcl.read(bb) > 0)//lee del canal, escribe en el buffer
-            {
-                System.out.println("1ºWhile");
-
-                while (bb.hasRemaining()) {
-                    byte byteActual = bb.get();//Leo el byte actual del bb
-                    System.out.println("2ºWhile");
-                    char letraActual = (char) byteActual; //Paso el byte actual a char
-                    if (Character.isLowerCase(letraActual)) {//Compruebo si es minúscula, si es minúscula, lo convierto a mayuscula
-                        letraActual = Character.toUpperCase(letraActual);
-                    }
-                    bb.flip();//hago flip para poder escribir en el bb
-                    bb.put((byte) letraActual);//escribo la mayuscula en el byte
-                }
-                bb.flip();//He escrito, ahora voy a leer
-                fce.write(bb);
-                bb.clear();// he leído, ahora voy a volver a escribir
-            }
-        }catch (IOException e)
+        try (FileChannel fce = FileChannel.open(Paths.get("upper.txt"),StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+             FileChannel fcl = FileChannel.open(Paths.get("lower.txt"), StandardOpenOption.READ))
         {
+            ByteBuffer bb = ByteBuffer.allocate(1);
+            char letra;
+
+            //Leo del canal de lectura para escribir en el buffer
+            while (fcl.read(bb)>0)
+            {
+               bb.flip();
+               letra = (char) bb.get();
+                //Ya he leido, ahora hay que escribir en el buffer
+
+                if(letra > 96 && letra < 123 ) {
+                    letra = (char) (letra - 32);
+                    bb.flip();
+                    bb.put((byte) letra);
+                }
+
+                bb.flip();
+                fce.write(bb);
+                bb.clear();
+            }
+
+
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
+
+
+
+
